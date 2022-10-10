f528bc1d49726e5afdebb2530009f5a5556b13a1
Гиоргий <73274199+giorgiy2003@users.noreply.github.com>
2022-10-10 18:06:08 +0300

    Delete TaskWithGINFramework2&&Postrgres directory

--- TaskWithGINFramework2&&Postrgres/internal/model/Person.go
+++
@@ -1,9 +0,0 @@
-package Model
-
-type Person struct {
-	Id        string `json:"id"`
-	Email     string `json:"email"`
-	Phone     string `json:"phone"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-}
